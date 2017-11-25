3ed7985738b702396bd156c459e154edbb2e262b
saori <saori@infratopnoMacBook-Air.local>
2017-11-25 17:14:36 +0900

    finish 5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,31 @@
 class BooksController < ApplicationController
+   before_action :authenticate_user!, except: [:top, :about]
   def top
+  end
+
+  def new
+      @book = Book.new
   end
 
   def index
   	  @book = Book.new
-  	  @books = Book.all
+  	  @books = Book.page(params[:page]).reverse_order
   end
 
   def create
-  	  book = Book.new(book_params)
-      book.save
-      redirect_to book_path(book.id), notice: "Book was successfully created."
+      @books = Book.page(params[:page]).reverse_order
+  	  @book = Book.new(book_params)
+      @book.user_id = current_user.id
+      if @book.save
+      redirect_to book_path(@book.id), notice: "すごい・・・君が熱中するわけだ。"
+      else
+      render 'index'
+      end
   end
 
   def show
   	  @book = Book.find(params[:id])
+      @booknew = Book.new
   end
 
   def edit
@@ -24,17 +35,26 @@ class BooksController < ApplicationController
   def destroy
       book = Book.find(params[:id])
       book.destroy
-      redirect_to books_path, notice: "Book was successfully destroyed."
+      redirect_to books_path, notice: "悲しいけどねアレ、削除したのよね！"
   end
 
   def update
-  	  book = Book.find(params[:id])
-  	  book.update(book_params)
-  	  redirect_to book_path, notice: "Book was successfully updated."
+  	  @book = Book.find(params[:id])
+  	  if @book.update(book_params)
+  	  redirect_to book_path, notice: "勝利の栄光を君に！"
+      else
+      render 'edit'
+      end
   end
 
   private
   def book_params
       params.require(:book).permit(:title, :body)
   end
+
+  private 
+  def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :books
+
+  attachment :profile_image
+
+  validates :name, length: { in: 2..20 }
+  validates :name, presence: true
+  validates :introduction, length: { maximum: 50 }
+end
